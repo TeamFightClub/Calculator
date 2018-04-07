c81e44a48466f8621c3b6949f9938e2c99b39935
AKSHAT <35437398+what-is-thy-bidding@users.noreply.github.com>
2018-04-06 20:17:55 -0400

    Update XpowY.java

--- codes/XpowY.java
+++ codes/XpowY.java
@@ -1,32 +1,36 @@
 package calculator;
 
+//Calculates X to the power of Y by splitting integer from decimal and then using taylor expansion for decimal part
+
 public class XpowY {
 	public static double answer=1;
 	public static double base;
 	public static double exponent ;
 
-	public static double xpowy(double answer) {
+	//calculates power of integer component
+	public static double xPowY(double answer) {
 		answer=answer*exponent;
 		return answer;
 	}
 	
 	
-	
+	//if the base is positive calculate x power y using Taylor expansion
 	public static double posBase(double base, double exp){
 		if (exp%1==0){
-			return EpowX.xpowerinty(base, exp);
+			return EpowX.xPowerInty(base, exp);
 		}
 		double log=Logarithms.Logarithms(base);
 		double expo_fac=exp%1;
 		double expo_int=exp-expo_fac;
-		double power=EpowX.xpowerinty(base, expo_int);
+		double power=EpowX.xPowerInty(base, expo_int);
 		double antilog=log*expo_fac;
-		answer=EpowX.ex(antilog);
+		answer=EpowX.calcExp(antilog);
 		answer=answer*power;
 		return answer;
 		
 	}
-	;
+	
+	//if the base is negative, calculate x power y using Taylor expansion
 	public static double negBase(double base,double exp){
 		double result;
 		if(base==0){
@@ -41,21 +45,22 @@ public class XpowY {
 				return 0;
 			}
 		}
+		
 		base=-base;
 		String xString = String.valueOf(base);
-		System.out.println(xString);
 		String[] parts = xString.split("\\.");
 		int count=parts[1].length();
-		int b=(int)EpowX.xpowerinty(10, count);
+		int b=(int)EpowX.xPowerInty(10, count);
 		int a=Integer.parseInt(parts[1])+Integer.parseInt(parts[0])*b;
-		System.out.println("a is "+a +" and b is "+b);
+		
+		//calculates greatest common denominator
 		int c=GCD.gcd(a,b);
 		a=a/c;
 		b=b/c;
 
 		if(b%2==0){
 			throw new IllegalArgumentException(
-                    "It's impossible to take even numberth root of a negative number.");
+                    "It's impossible to take even number root of a negative number.");
 		}
 		else if(a%2==0){
 			result=posBase(base,exp);
