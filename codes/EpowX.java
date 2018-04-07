074a97a0a723391569f196b99b07e32e46f134e3
AKSHAT <35437398+what-is-thy-bidding@users.noreply.github.com>
2018-04-06 20:14:43 -0400

    Update EpowX.java

--- codes/EpowX.java
+++ codes/EpowX.java
@@ -1,15 +1,15 @@
 package calculator;
-import Exceptions;
-import java.util.Scanner;
 
+//calculates e to the power of x by separating integer and decimal and recursively call power function
 public class EpowX {
+	public static final double Euler_Number=2.7182818284590452353602874713527;
 	
-	public static double ex(double exponent){
+	//separates exponent into integer and decimal part
+	public static double calcExp(double exponent){
 		double answer=1;
 		double fact=1;
 		double exp=1;
 		
-		// the previous code was only applicable to small values of exponent.
 		double exponent_fac=exponent%1; // chopping off into integer part and decimal part
 		double exponent_int=exponent-exponent_fac;// absolute value of the integer part.
 		
@@ -18,39 +18,16 @@ public class EpowX {
 			exp=exp*exponent_fac;
 			answer= answer + (exp/fact);
 		}
-		double result=xpowerinty(2.7182818284590452353602874713527,exponent_int); 
+		double result=xPowerInty(Euler_Number,exponent_int); 
 		return result*answer;
 		
 		
 	}
 	
-	/*public static void main (String[] args){
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Value of exponent :: ");
-		Double exponent= sc.nextDouble();
-		sc.close();	
-		double answer=1;
-		double fact=1;
-		double exp=1;
-		
-		// the previous code was only applicable to small values of exponent.
-		double exponent_fac=exponent%1; // chopping off into integer part and decimal part
-		double exponent_int=exponent-exponent_fac;// absolute value of the integer part.
-		
-		for(int i=1;i<100;i++){
-			fact=fact*i;
-			exp=exp*exponent_fac;
-			answer= answer + (exp/fact);
-		}
-		double result=xpowerinty(2.718281828,exponent_int); 
-		System.out.println(result*answer);
-		System.out.println(Math.pow(2.718281828, exponent));
-	}*/
 	
 	
-	
-	
-	public static double xpowerinty(double x, double y) {
+	//performs x to the power of integer y
+	public static double xPowerInty(double x, double y) {
 	    y=(int)y;
 		if ( y== 0) {
 	        return 1.0;
@@ -59,13 +36,13 @@ public class EpowX {
 	        // Negative power.
 	        if (x == 0) {
 	            throw new IllegalArgumentException(
-	                    "It's impossible to raise 0 to the power of a negative number");
+	                         "It's impossible to raise 0 to the power of a negative number");
 	        }
-	        return 1 / xpowerinty(x, -y);
+	        return 1 / xPowerInty(x, -y);
 	    } else {
 	        // Positive power
 
-	        double temp = xpowerinty(x,y/2);
+	        double temp = xPowerInty(x,y/2);
 	        if (y % 2 == 1) {
 	            // Odd n
 	            return x * temp * temp;
